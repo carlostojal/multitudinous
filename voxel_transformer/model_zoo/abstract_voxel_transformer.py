2a2fa6a3bc7bb7c4b6e4a63facb914e01f8fe432
carlostojal <carlos.tojal@hotmail.com>
2023-10-03 15:35:13 +0100

    implement concurrent feature extraction to reduce latency

--- voxel_transformer/model_zoo/abstract_voxel_transformer.py
+++ voxel_transformer/model_zoo/abstract_voxel_transformer.py
@@ -2,6 +2,7 @@ import torch
 
 from abc import abstractmethod, ABC
 from typing import Type, TypeVar, Generic
+from threading import Thread
 
 # import the point cloud backbones
 from voxel_transformer.backbones.point_cloud.point_cloud_backbone import PointCloudBackbone
@@ -27,11 +28,30 @@ class AbstractVoxelTransformer(ABC, torch.nn.Module, Generic[PointCloudBackboneT
 
     def forward(self, point_cloud, rgbd):
 
-        # extract point cloud features using the point cloud backbone
-        point_cloud_features = self.point_cloud_backbone(point_cloud)
+        point_cloud_features = None
+        rgbd_features = None
 
-        # extract rgbd features using the rgbd backbone
-        rgbd_features = self.rgbd_backbone(rgbd)
+        # a routine to extract point cloud features
+        def run_point_cloud_backbone():
+            nonlocal point_cloud_features
+            point_cloud_features = self.point_cloud_backbone(point_cloud)
+
+        # a routine to extract rgbd features
+        def run_rgbd_backbone():
+            nonlocal rgbd_features
+            rgbd_features = self.rgbd_backbone(rgbd)
+
+        # create a thread to extract point cloud features and another to extract rgbd features
+        point_cloud_thread = Thread(target=run_point_cloud_backbone)
+        rgbd_thread = Thread(target=run_rgbd_backbone)
+
+        # start the threads concurrently
+        point_cloud_thread.start()
+        rgbd_thread.start()
+
+        # wait for the threads
+        point_cloud_thread.join()
+        rgbd_thread.join()
 
         # fuse the features using the neck
         fused_features = self.neck(point_cloud_features, rgbd_features)
