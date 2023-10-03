f6631594910b8dad76bc1253ab98c603bef97143
carlostojal <carlos.tojal@hotmail.com>
2023-10-03 15:24:36 +0100

    implement a placeholder resnet forward

--- voxel_transformer/backbones/rgbd/resnet50.py
+++ voxel_transformer/backbones/rgbd/resnet50.py
@@ -4,4 +4,7 @@ from voxel_transformer.backbones.rgbd.rgbd_backbone import RGBDBackbone
 # RGBD backbone using the "ResNet 50" architecture - convolutional.
 class ResNet50(RGBDBackbone):
 
-    pass
+    def forward(self, rgbd):
+        print("ResNet 50 forward")
+
+        pass
