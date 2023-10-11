bf834bab67127204b01d41999ff46ccf9875c09e
carlostojal <carlos.tojal@hotmail.com>
2023-10-11 22:52:24 +0100

    implement the cbam forward pass

--- voxel_transformer/backbones/rgbd/attention/convolutional_block_attention_module.py
+++ voxel_transformer/backbones/rgbd/attention/convolutional_block_attention_module.py
@@ -19,3 +19,12 @@ class CBAM(torch.nn.Module):
         # -- spatial attention module --
         self.conv1 = torch.nn.Conv2d(in_channels=2, out_channels=1, kernel_size=7)
 
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        f_avg = self.avgpool(x)
+        f_max = self.maxpool(x)
+
+        f_avg1 = self.sigmoid(self.fc2(self.fc1(f_avg)))
+        f_max1 = self.fc2(self.fc1(f_max))
+
+        # concatenating on channel 0 generates a tensor of 2xWxH
+        return self.sigmoid(self.conv1(torch.cat((f_avg, f_max), dim=0)))
