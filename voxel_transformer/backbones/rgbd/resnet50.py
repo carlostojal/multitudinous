312ba6a207a3364ef357d66e6905b3d8ed5e7473
carlostojal <carlos.tojal@hotmail.com>
2023-10-12 21:01:55 +0100

    fix problems regarding dimensions

--- voxel_transformer/backbones/rgbd/resnet50.py
+++ voxel_transformer/backbones/rgbd/resnet50.py
@@ -3,40 +3,55 @@ from voxel_transformer.backbones.rgbd.rgbd_backbone import RGBDBackbone
 
 # RGBD backbone using the "ResNet 50" architecture - convolutional.
 
+
+class Shortcut(torch.nn.Module):
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 2):
+        super().__init__()
+
+        # "when the shortcuts go across feature maps of two sizes, they are performed with a stride of 2."
+        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(out_channels)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.bn1(self.conv1(x))
+
+
 # residual block class
 class ResBlock(torch.nn.Module):
 
-    def __init__(self, input_size: int, first_layer_stride: int = 1):
+    def __init__(self, in_channels: int, true_in_channels: int, stride: int = 1):
         super().__init__()
 
         self.expansion: int = 4
 
-        self.input_size: int = input_size
+        self.in_channels: int = in_channels
 
-        self.block = BottleneckBlock(input_size, first_layer_stride)
+        self.stride = stride
+
+        self.block = BottleneckBlock(in_channels, true_in_channels, stride)
+
+        self.sigmoid = torch.nn.Sigmoid()
 
     def forward(self, x: torch.Tensor):
 
         # save the residual value
-        x_in: torch.Tensor = x
+        residual: torch.Tensor = x
 
         # do the forward pass
         x = self.block(x)
 
-        # handle shape differences
-        if self.input_size * self.expansion != x_in.size(dim=0):
-            # adapt dimensions with a 1x1 conv layer
-            x = torch.nn.Conv2d(in_channels=x.size(dim=0), out_channels=self.input_size * self.expansion, kernel_size=1)
+        # adapt dimensions with a 1x1 conv layer
+        shortcut = Shortcut(residual.size(dim=1), x.size(dim=1), stride=self.stride)
+        residual = shortcut(residual)
 
         # concatenate the new feature map and the residual connection
-        return x + x_in
-
+        return self.sigmoid(x + residual)
 
 
 # bottleneck block class (1x1 -> 3x3 -> 1x1 convolutions)
 class BottleneckBlock(torch.nn.Module):
 
-    def __init__(self, input_size: int, first_layer_stride: int = 1):
+    def __init__(self, in_channels: int, true_in_channels: int, stride: int = 1):
         super().__init__()
 
         # outputs are 4x times larger than inputs
@@ -44,35 +59,40 @@ class BottleneckBlock(torch.nn.Module):
 
         self.relu = torch.nn.ReLU()
 
+        # "Downsampling is performed by conv3_1, conv4_1, and conv5_1 with a stride of 2."
+
         # 1x1
-        self.bn1 = torch.nn.BatchNorm2d(input_size)
-        self.conv1 = torch.nn.Conv2d(in_channels=input_size, out_channels=input_size, kernel_size=1,
-                                     stride=first_layer_stride)
+        self.conv1 = torch.nn.Conv2d(in_channels=true_in_channels, out_channels=in_channels, kernel_size=1,
+                                     stride=stride, bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(in_channels)
 
         # 3x3
-        self.bn2 = torch.nn.BatchNorm2d(input_size)
-        self.conv2 = torch.nn.Conv2d(in_channels=input_size, out_channels=input_size, kernel_size=3, stride=1)
+        self.conv2 = torch.nn.Conv2d(in_channels=in_channels, out_channels=in_channels, kernel_size=3, stride=1,
+                                     padding=1, bias=False)
+        self.bn2 = torch.nn.BatchNorm2d(in_channels)
 
         # 1x1
-        self.bn3 = torch.nn.BatchNorm2d(input_size)
-        self.conv3 = torch.nn.Conv2d(in_channels=input_size, out_channels=input_size * self.expansion,
-                                     kernel_size=1,
-                                     stride=1)
+        self.conv3 = torch.nn.Conv2d(in_channels=in_channels, out_channels=in_channels * self.expansion,
+                                     kernel_size=1, stride=1, bias=False)
+        self.bn3 = torch.nn.BatchNorm2d(in_channels * self.expansion)
 
     def forward(self, x):
+
+        # "We adopt batch normalization (BN) [16] right after each convolution and before activation, following [16]."
+        x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
-        x = self.conv1(x)
 
+        x = self.conv2(x)
         x = self.bn2(x)
         x = self.relu(x)
-        x = self.conv2(x)
 
+        x = self.conv3(x)
         x = self.bn3(x)
         x = self.relu(x)
-        x = self.conv3(x)
 
         return x
+
 
 class ResNet50(RGBDBackbone):
 
@@ -91,41 +111,58 @@ class ResNet50(RGBDBackbone):
         # convolutional block implementations (4 blocks)
         self.res_blocks: list[ResBlock] = []
 
+        # the first block has layers with 64 input size
         input_size: int = 64
+        last_input_size: int = input_size
+        last_output_size: int = 64
+
+        block_idx: int = 0
 
         # create the convolutional blocks
         for block_count in self.block_counts:
 
-            # the stride i s 1 on the first layer of each block
-            stride = 2
+            for _ in range(block_count):
 
-            for block in range(block_count):
+                # "(...) when the shortcuts go across feature maps of two sizes, they are performed with a stride of 2."
+                if last_input_size != input_size:
+                    stride = 2
+                else:
+                    stride = 1
+
+                new_block = ResBlock(input_size, last_output_size, stride)
+
+                # save the previous input size
+                last_input_size = input_size
+
+                last_output_size = new_block.in_channels * new_block.expansion
 
                 # create a bottleneck block
-                self.res_blocks.append(ResBlock(input_size, first_layer_stride=stride))
+                self.res_blocks.append(new_block)
 
-                stride = 1
-
+            # at each block, the input size doubles
             input_size *= 2
 
+            block_idx += 1
+
+        self.block = torch.nn.Sequential(*self.res_blocks)
+
         self.avgpool = torch.nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = torch.nn.Linear(1024, out_feature_len)
+        self.fc = torch.nn.Linear(1, out_feature_len)
 
         self.softmax = torch.nn.Softmax2d()
 
     def forward(self, rgbd: torch.Tensor) -> torch.Tensor:
 
         # input shape must be 3x224x224
-        if rgbd.size() != (3, 224, 224):
-            raise RuntimeError("Invalid RGB-D tensor shape: must be 3x224x224! ({} was provided)".format(rgbd.size()))
+        if rgbd.size() != (1, 4, 224, 224):
+            raise ValueError("Invalid RGB-D tensor shape: must be 4x224x224! ({} was provided)".format(rgbd.size()))
 
-        x = self.bn1(rgbd)
-        x = self.conv1(x)
+        x = self.conv1(rgbd)
+        x = self.bn1(x)
 
         x = self.maxpool(x)
 
-        for block in self.res_blocks:
-            x = block(x)
+        x = self.block(x)
 
         x = self.avgpool(x)
 
