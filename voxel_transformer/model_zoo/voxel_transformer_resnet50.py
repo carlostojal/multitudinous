06c12809624fdcc40f883ee9ea34d1c4d3bcaa97
carlostojal <carlos.tojal@hotmail.com>
2023-10-17 09:04:30 +0100

    update the imports

--- voxel_transformer/model_zoo/voxel_transformer_resnet50.py
+++ voxel_transformer/model_zoo/voxel_transformer_resnet50.py
@@ -1,14 +1,10 @@
-from typing import Type
-
-import torch
-
-from voxel_transformer.model_zoo.abstract_voxel_transformer import AbstractVoxelTransformer, PointCloudBackboneT
+from voxel_transformer.model_zoo.abstract_voxel_transformer import AbstractVoxelTransformer
 
 # import the point cloud backbone
 from voxel_transformer.backbones.point_cloud.point_transformer import PointTransformer
 
 # import the rgbd backbone
-from voxel_transformer.backbones.rgbd.resnet50 import ResNet50
+from voxel_transformer.backbones.rgbd.resnet.resnet50 import ResNet50
 
 # import the neck
 from voxel_transformer.necks.concat import ConcatNeck
