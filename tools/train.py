6a0b5fb7d55d10b60917b91571fe33fb6287a507
carlostojal <carlos.tojal@hotmail.com>
2023-10-17 17:25:00 +0100

    add the new model to the training script for testing purposes

--- tools/train.py
+++ tools/train.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append(".")
 
 from voxel_transformer.model_zoo.voxel_transformer_resnet50 import VoxelTransformer_ResNet50
+from voxel_transformer.model_zoo.voxel_transformer_seresnet50 import VoxelTransformer_SEResNet50
 
 # Run the training
 
@@ -15,6 +16,12 @@ if __name__ == "__main__":
     if torch.cuda.is_available():
         device = torch.device("cuda")
 
+    # generate random input tensors
+    random_pcl = torch.rand((1, 10000)).to(device)
+    random_rgbd = torch.rand((1, 4, 224, 224)).to(device)
+
+    print("------- ResNet-50 -------")
+
     # initialize the model
     model = VoxelTransformer_ResNet50()
 
@@ -23,11 +30,16 @@ if __name__ == "__main__":
 
     print(model)
 
-    # TODO
-
-    # generate random input tensors
-    random_pcl = torch.rand((1, 10000)).to(device)
-    random_rgbd = torch.rand((1, 4, 224, 224)).to(device)
-
     # test forward pass with random tensors (with correct dimensions)
     out = model(random_pcl, random_rgbd)
+
+
+    print("------- SE-ResNet-50 -------")
+
+    model1 = VoxelTransformer_SEResNet50()
+    model1.to(device)
+
+    print(model1)
+
+    out1 = model1(random_pcl, random_rgbd)
+
