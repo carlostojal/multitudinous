d0470b725074c38897c8fd626c794b67125ddabd
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-02-29 17:02:20 +0000

    refactor from an abstract class to a model builder

--- multitudinous/model_zoo/multitudinous.py
+++ multitudinous/model_zoo/multitudinous.py
@@ -1,30 +1,18 @@
 import torch
-
-from abc import abstractmethod, ABC
-from typing import Type, TypeVar, Generic
+from torch import nn
 from threading import Thread
 
-# import the point cloud backbones
-from multitudinous.backbones.point_cloud.point_cloud_backbone import PointCloudBackbone
-
-# import the rgbd backbones
-from multitudinous.backbones.rgbd.rgbd_backbone import RGBDBackbone
-
-# import the necks
-from multitudinous.necks.neck import Neck
-
-# import the heads
-from multitudinous.heads.head import Head
-
-# define abstract types bound to the abstract networks
-PointCloudBackboneT = TypeVar('PointCloudBackboneT', bound=PointCloudBackbone)
-RGBDBackboneT = TypeVar('RGBDBackboneT', bound=RGBDBackbone)
-NeckT = TypeVar('NeckT', bound=Neck)
-HeadT = TypeVar('HeadT', bound=Head)
-
-
 # Abstract voxel transformer implementation. Can have any combination of backbones, neck and heads.
-class AbstractVoxelTransformer(ABC, torch.nn.Module, Generic[PointCloudBackboneT, RGBDBackboneT, NeckT, HeadT]):
+class Multitudinous(torch.nn.Module):
+
+    def __init__(self, img_backbone: nn.Module, point_cloud_backbone: nn.Module, neck: nn.Module, head: nn.Module) -> None:
+        super().__init__()
+
+        self.img_backbone = img_backbone
+        self.point_cloud_backbone = point_cloud_backbone
+        self.neck = neck
+        self.head = head
+
 
     def forward(self, point_cloud: torch.Tensor, rgbd: torch.Tensor) -> torch.Tensor:
 
@@ -37,13 +25,13 @@ class AbstractVoxelTransformer(ABC, torch.nn.Module, Generic[PointCloudBackboneT
             point_cloud_features = self.point_cloud_backbone(point_cloud)
 
         # a routine to extract rgbd features
-        def run_rgbd_backbone():
+        def run_img_backbone():
             nonlocal rgbd_features
-            rgbd_features = self.rgbd_backbone(rgbd)
+            rgbd_features = self.img_backbone(rgbd)
 
         # create a thread to extract point cloud features and another to extract rgbd features
         point_cloud_thread = Thread(target=run_point_cloud_backbone)
-        rgbd_thread = Thread(target=run_rgbd_backbone)
+        rgbd_thread = Thread(target=run_img_backbone)
 
         # start the threads concurrently
         point_cloud_thread.start()
---
+++ multitudinous/utils/model_builder.py
@@ -0,0 +1,46 @@
+import torch
+from multitudinous.model_zoo import img_backbones, point_cloud_backbones, necks, heads
+from multitudinous.model_zoo.multitudinous import Multitudinous
+
+# ensemble the multitudinous model
+def build_multitudinous(img_backbone: str, point_cloud_backbone: str, neck: str, head: str,
+                        img_backbone_weights_path: str = None, point_cloud_backbone_weights_path: str = None) -> Multitudinous:
+    
+    # get the image backbone
+    build_img_backbone(img_backbone, img_backbone_weights_path)
+
+    # get the point cloud backbone
+    build_point_cloud_backbone(point_cloud_backbone, point_cloud_backbone_weights_path)
+
+    # get the neck
+    if neck not in necks:
+        raise ValueError(f'Neck {neck} not found. Available necks are {list(necks.keys())}.')
+    neck = necks[neck]
+
+    # get the head
+    if head not in heads:
+        raise ValueError(f'Head {head} not found. Available heads are {list(heads.keys())}.')
+    head = heads[head]
+
+    # create the model
+    model = Multitudinous(img_backbone, point_cloud_backbone, neck, head)
+
+    return model
+
+# build the image backbone
+def build_img_backbone(img_backbone: str, weights_path: str = None) -> torch.nn.Module:
+    if img_backbone not in img_backbones:
+        raise ValueError(f'Image backbone {img_backbone} not found. Available image backbones are {list(img_backbones.keys())}.')
+    img_b = img_backbones[img_backbone]
+    if weights_path is not None:
+        img_b.load_state_dict(torch.load(weights_path))
+    return img_b
+
+# build the point cloud backbone
+def build_point_cloud_backbone(point_cloud_backbone: str, weights_path: str = None) -> torch.nn.Module:
+    if point_cloud_backbone not in point_cloud_backbones:
+        raise ValueError(f'Point cloud backbone {point_cloud_backbone} not found. Available point cloud backbones are {list(point_cloud_backbones.keys())}.')
+    point_cloud_b = point_cloud_backbones[point_cloud_backbone]
+    if weights_path is not None:
+        point_cloud_b.load_state_dict(torch.load(weights_path))
+    return point_cloud_b
