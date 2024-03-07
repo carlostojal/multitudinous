c5195afd82dd5d1ae5efcbd25b7ef6593ece5b18
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-03-07 10:13:01 +0000

    implement the pixel loss as mean squared error

--- multitudinous/loss_fns.py
+++ multitudinous/loss_fns.py
@@ -15,12 +15,4 @@ def pixel_loss(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     if pred.shape != target.shape:
         raise ValueError(f'The input tensors must have the same shape. Got {pred.shape} and {target.shape}.')
     
-    num_pixels = pred.shape[0] * pred.shape[1]
-
-    # calculate the difference
-    diff = pred - target
-
-    # calculate the l2 norm
-    l2 = torch.linalg.matrix_norm(diff, ord=2)
-
-    return l2
+    return torch.nn.MSELoss()(pred, target)
