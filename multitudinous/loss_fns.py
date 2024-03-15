18595384d5033608ab7b64d613039f0a94cc9c4e
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-03-15 16:36:14 +0000

    implement log10 metric

--- multitudinous/loss_fns.py
+++ multitudinous/loss_fns.py
@@ -33,3 +33,20 @@ def are(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         raise ValueError(f'The input tensors must have the same shape. Got {pred.shape} and {target.shape}.')
     
     return torch.mean(torch.abs(pred - target) / target)
+
+# log10 error
+def log10_error(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+
+    # squeeze the tensors
+    pred = torch.squeeze(pred)
+    target = torch.squeeze(target)
+
+    # verify that the tensors are 2-d
+    if len(pred.shape) != 2 or len(target.shape) != 2:
+        raise ValueError('The input tensors must be 2-d.')
+    
+    # compare dimensions
+    if pred.shape != target.shape:
+        raise ValueError(f'The input tensors must have the same shape. Got {pred.shape} and {target.shape}.')
+    
+    return torch.mean(torch.log10(torch.abs(pred - target) + 1e-9))
