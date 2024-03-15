52e8ffa2e52f3a8aa20538ec542a09b83b707ea3
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-03-15 17:59:50 +0000

    fix pretrainer metrics

--- multitudinous/loss_fns.py
+++ multitudinous/loss_fns.py
@@ -17,8 +17,8 @@ def rmse(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     
     return torch.sqrt(torch.nn.MSELoss()(pred, target))
 
-# mean absolute error (MAE)
-def mae(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+# absolute relative error (REL)
+def rel(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
 
     # squeeze the tensors
     pred = torch.squeeze(pred)
@@ -32,28 +32,11 @@ def mae(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
     if pred.shape != target.shape:
         raise ValueError(f'The input tensors must have the same shape. Got {pred.shape} and {target.shape}.')
     
-    target = torch.where(target == 0, target, 1e-9) # avoid division by zero
-    return torch.mean(torch.abs(pred - target) / target)
+    denominator = torch.where(torch.abs(target) > 1e-9, torch.abs(target), torch.ones_like(target))
+    return torch.mean(torch.div(torch.abs(target - pred), denominator))
 
-# log10 error
-def log10_error(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
-
-    # squeeze the tensors
-    pred = torch.squeeze(pred)
-    target = torch.squeeze(target)
-
-    # verify that the tensors are 2-d
-    if len(pred.shape) != 2 or len(target.shape) != 2:
-        raise ValueError('The input tensors must be 2-d.')
-    
-    # compare dimensions
-    if pred.shape != target.shape:
-        raise ValueError(f'The input tensors must have the same shape. Got {pred.shape} and {target.shape}.')
-    
-    return torch.mean(torch.log10(torch.abs(pred - target) + 1e-9))
-
-# huber loss
-def huber_loss(pred: torch.Tensor, target: torch.Tensor, delta: float = 1.0) -> torch.Tensor:
+# delta error
+def delta(pred: torch.Tensor, target: torch.Tensor, delta: float = 1.0) -> torch.Tensor:
 
     # squeeze the tensors
     pred = torch.squeeze(pred)
@@ -67,5 +50,5 @@ def huber_loss(pred: torch.Tensor, target: torch.Tensor, delta: float = 1.0) -> 
     if pred.shape != target.shape:
         raise ValueError(f'The input tensors must have the same shape. Got {pred.shape} and {target.shape}.')
     
-    # calculate the loss
-    return torch.nn.SmoothL1Loss(reduction='none')(pred, target)
+    mask = torch.le(torch.abs(pred - target), delta).float()
+    return torch.mean(mask)
