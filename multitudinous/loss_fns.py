92eb9d0a44927538485dd43dbb0f9b3a42a9d3ce
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-03-15 16:33:34 +0000

    implement absolute relative error

--- multitudinous/loss_fns.py
+++ multitudinous/loss_fns.py
@@ -1,6 +1,6 @@
 import torch
 
-# pixel loss function
+# rooted mean squared error (RMSE)
 def rmse(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
 
     # squeeze the tensors
@@ -16,3 +16,20 @@ def rmse(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         raise ValueError(f'The input tensors must have the same shape. Got {pred.shape} and {target.shape}.')
     
     return torch.sqrt(torch.nn.MSELoss()(pred, target))
+
+# absolute relative error (ARE)
+def are(pred: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+
+    # squeeze the tensors
+    pred = torch.squeeze(pred)
+    target = torch.squeeze(target)
+
+    # verify that the tensors are 2-d
+    if len(pred.shape) != 2 or len(target.shape) != 2:
+        raise ValueError('The input tensors must be 2-d.')
+    
+    # compare dimensions
+    if pred.shape != target.shape:
+        raise ValueError(f'The input tensors must have the same shape. Got {pred.shape} and {target.shape}.')
+    
+    return torch.mean(torch.abs(pred - target) / target)
