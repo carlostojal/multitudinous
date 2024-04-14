2049889e5f913a572209acc326507045655a45ed
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-04-14 20:37:09 +0100

    vit embedding implemented

--- multitudinous/backbones/image/vit_embedding.py
+++ multitudinous/backbones/image/vit_embedding.py
@@ -10,24 +10,26 @@ class ViT_Embedding(torch.nn.Module):
     - feature_dim (torch.Size): the dimension of the input feature map
     - embedding_dim (int): the dimension of the embedding
     """
-    def __init__(self, patch_size: int = 16, feature_dim: torch.Size = torch.Size(2048, 15, 20), embedding_dim: int = 512) -> None:
+    def __init__(self, patch_size: int = 16, embedding_dim: int = 768, feature_dim: torch.Size = torch.Size(1, 2048, 15, 20)) -> None:
         super().__init__()
         
         # initialize the patch size
         self.patch_size = patch_size
         if self.patch_size < 1:
             raise ValueError("Patch size should be greater or equal to 1")
-
-        # initialize the feature dimension - dimension of the feature map
-        self.feature_dim = feature_dim
-        num_features = self.feature_dim[0] * self.feature_dim[1] * self.feature_dim[2] # total number of elements on the feature map
         
         # initialize the embedding dimension
         self.embedding_dim = embedding_dim
         if self.embedding_dim < 1:
             raise ValueError("Embedding dimension should be greater or equal to 1")
-        # initialize the linear layer
-        self.fc = nn.Linear(num_features, self.embedding_dim)
+        
+        # initialize the feature dimension
+        self.feature_dim = feature_dim
+        
+        # initialize the convolutional layer
+        self.conv = nn.Conv2d(in_channels=self.feature_dim[1], out_channels=self.embedding_dim, kernel_size=self.patch_size, stride=self.patch_size)
+        
+
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
@@ -40,9 +42,28 @@ class ViT_Embedding(torch.nn.Module):
         - torch.Tensor: the embedded feature map
         """
 
-        # verify the input tensor shape
-        if x.shape[1:] != self.feature_dim:
-            raise ValueError(f"Input feature map shape {x.shape[1:]} does not match the expected feature dimension {self.feature_dim}")
-        
-        pass
+        # batch size, channels, height, width
+        n, c, h, w = x.shape
 
+        # verify the input tensor shape
+        if x.shape != self.feature_dim:
+            raise ValueError(f"Input tensor shape should be {self.feature_dim}, but got {x.shape}")
+
+        # calculate the number of patches in the height and width
+        n_h = h // self.patch_size
+        n_w = w // self.patch_size
+
+        # apply the convolutional layer to the input tensor (embedding the feature map)
+        # (n, c, h, w) to (n, embedding_dim, n_h, n_w)
+        x = self.conv(x)
+
+        # reshape to (n, embedding_dim, n_h * n_w). sequence length = n_h * n_w = number of patches
+        x = x.reshape(n, self.embedding_dim, n_h * n_w)
+
+
+        # transpose to (n, n_h * n_w, embedding_dim)
+        # (batch size, sequence length, embedding dimension)
+        x = x.permute(0, 2, 1)
+
+        return x
+        
