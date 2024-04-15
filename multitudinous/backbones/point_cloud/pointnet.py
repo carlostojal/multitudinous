54106e17a186ef0b6cc16113921ae6e71d79e2a9
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-04-15 19:38:04 +0100

    implement t-net

---
+++ multitudinous/backbones/point_cloud/pointnet.py
@@ -0,0 +1,60 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+
+class TNet(nn.Module):
+    """
+    Transformation Network
+    """
+
+    def __init__(self, in_dim: int = 64) -> None:
+        super().__init__()
+
+        self.in_dim = in_dim
+
+        self.conv1 = nn.Conv1d(in_dim, 64, 1)
+        self.conv2 = nn.Conv1d(64, 128, 1)
+        self.conv3 = nn.Conv1d(128, 1024, 1)
+
+        self.fc1 = nn.Linear(1024, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, in_dim**2)
+        self.relu = nn.ReLU()
+
+        self.bn1 = nn.BatchNorm1d(64)
+        self.bn2 = nn.BatchNorm1d(128)
+        self.bn3 = nn.BatchNorm1d(1024)
+        self.bn4 = nn.BatchNorm1d(512)
+        self.bn5 = nn.BatchNorm1d(256)
+
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass of the Transformation Network
+
+        Args:
+        - x (torch.Tensor): the input point cloud
+
+        Returns:
+        - torch.Tensor: the output of the Transformation Network
+        """
+
+        # MLP
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.relu(self.bn2(self.conv2(x)))
+        x = self.relu(self.bn3(self.conv3(x)))
+
+        # max pooling
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+
+        # FC layers
+        x = self.relu(self.bn4(self.fc1(x)))
+        x = self.relu(self.bn5(self.fc2(x)))
+        x = self.fc3(x)
+
+        # add identity matrix
+        x += torch.eye(self.in_dim).view(1, self.in_dim**2).repeat(x.size(0), 1)
+        x = x.view(-1, self.in_dim, self.in_dim)
+
+        return x
