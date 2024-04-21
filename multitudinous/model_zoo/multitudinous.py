9d50df905483c5cb28854d0631e71ab84e14d3b0
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-04-21 13:16:02 +0100

    update the basic structure of multitudinous

--- multitudinous/model_zoo/multitudinous.py
+++ multitudinous/model_zoo/multitudinous.py
@@ -1,52 +1,51 @@
 import torch
 from torch import nn
-from multitudinous.necks.concat import ConcatNeck
-from multitudinous.heads.deconv import DeconvHead
 from threading import Thread
 
-# Abstract voxel transformer implementation. Can have any combination of backbones, neck and heads.
+# Multitudinous implementation
 class Multitudinous(torch.nn.Module):
 
-    def __init__(self, img_backbone: nn.Module, point_cloud_backbone: nn.Module) -> None:
+    def __init__(self, img_backbone: nn.Module, point_cloud_backbone: nn.Module, neck: nn.Module, head: nn.Module) -> None:
         super().__init__()
 
         self.img_backbone = img_backbone
         self.point_cloud_backbone = point_cloud_backbone
-        self.neck = ConcatNeck()
-        self.head = DeconvHead()
+        self.neck = neck
+        self.head = head
 
 
     def forward(self, point_cloud: torch.Tensor, rgbd: torch.Tensor) -> torch.Tensor:
 
-        point_cloud_features = None
+        # initialize the features
         rgbd_features = None
-
-        # a routine to extract point cloud features
-        def run_point_cloud_backbone():
-            nonlocal point_cloud_features
-            point_cloud_features = self.point_cloud_backbone(point_cloud)
+        point_cloud_features = None
 
         # a routine to extract rgbd features
         def run_img_backbone():
             nonlocal rgbd_features
             rgbd_features = self.img_backbone(rgbd)
 
+        # a routine to extract point cloud features
+        def run_point_cloud_backbone():
+            nonlocal point_cloud_features
+            point_cloud_features = self.point_cloud_backbone(point_cloud)
+
         # create a thread to extract point cloud features and another to extract rgbd features
-        point_cloud_thread = Thread(target=run_point_cloud_backbone)
         rgbd_thread = Thread(target=run_img_backbone)
+        point_cloud_thread = Thread(target=run_point_cloud_backbone)
 
         # start the threads concurrently
-        point_cloud_thread.start()
         rgbd_thread.start()
+        point_cloud_thread.start()
 
         # wait for the threads
-        point_cloud_thread.join()
         rgbd_thread.join()
+        point_cloud_thread.join()
 
         # fuse the features using the neck
-        fused_features = self.neck(point_cloud_features, rgbd_features)
+        rgbd_embeddings, point_cloud_embeddings = self.neck(point_cloud_features, rgbd_features)
 
-        # reconstruct the occupancy using the occupancy head
-        occupancy = self.head(fused_features)
+        # decode the end task using the head, be it grid or cones
+        out = self.head(rgbd_embeddings, point_cloud_embeddings)
 
-        return occupancy
+        return out
