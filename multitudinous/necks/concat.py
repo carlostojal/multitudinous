adf197e103325afb6e3756cb625e1730af4f609b
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-04-21 13:27:04 +0100

    remove unused confusing code

--- multitudinous/necks/concat.py
+++
@@ -1,9 +0,0 @@
-from torch import nn, Tensor, cat
-
-class ConcatNeck(nn.Module):
-
-    def __init__(self) -> None:
-        super().__init__()
-
-    def forward(self, x: Tensor, y: Tensor) -> Tensor:
-        return cat(x, y)
