2450b054313aeed22760fb8a0f184e1e6f584cd8
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-04-22 22:57:20 +0100

    add the cross-entropy function to the loss function index

--- multitudinous/loss_index.py
+++ multitudinous/loss_index.py
@@ -1,7 +1,9 @@
 from .loss_fns import rmse, rel, delta
+import torch.nn.functional as F
 
 img_loss_fns = {
     'rmse': rmse,
     'rel': rel,
-    'delta': delta
+    'delta': delta,
+    'cross_entropy': F.cross_entropy
 }
