3180c5f0a307efa587dd98fb200f78071dda959c
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-04-25 13:07:47 +0100

    add the point cloud segmentation dataset to the dataset index

--- multitudinous/dataset_index.py
+++ multitudinous/dataset_index.py
@@ -1,7 +1,9 @@
 from multitudinous.datasets.TUM_RGBD import TUM_RGBD
 from multitudinous.datasets.CARLA_RGBD import CARLA_RGBD
+from multitudinous.datasets.CARLA_PCL_SEG import CARLA_PCL_SEG
 
 datasets = {
     'tum_rgbd': TUM_RGBD,
-    "carla_rgbd": CARLA_RGBD
+    "carla_rgbd": CARLA_RGBD,
+    "carla_pcl_seg": CARLA_PCL_SEG
 }
