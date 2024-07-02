8432e4a8ab6bc264532720134f28045c6b6655eb
Carlos Tojal <carlos.c.tojal@gmail.com>
2024-07-02 12:26:15 +0100

    update the config paths and add missing configs

--- tools/train.py
+++ tools/train.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append(".")
 
 from multitudinous.utils.model_builder import build_multitudinous
-from ..configs.model.ModelConfig import ModelConfig
+from multitudinous.configs.model.ModelConfig import ModelConfig
 
 # Run the training
 
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     # parse command line arguments
     parser = argparse.ArgumentParser(description='Train the model')
-    parser.add_argument('--config', type=str, default='multitudinous/model_zoo/configs/se_resnet50-pointnet.yaml', help='Path to the model YAML configuration file.')
+    parser.add_argument('--config', type=str, default='multitudinous/configs/model/se_resnet50-pointnet.yaml', help='Path to the model YAML configuration file.')
     parser.add_argument('--img_backbone_weights', type=str, default=None, help='Path to the weights of the image backbone')
     parser.add_argument('--point_cloud_backbone_weights', type=str, default=None, help='Path to the weights of the point cloud backbone')
     parser.add_argument('--output', type=str, default='output', help='Path to save the model')
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     print(config)
 
     # build the model
-    model = build_multitudinous(config.img_backbone, config.point_cloud_backbone, args.img_backbone_weights, args.point_cloud_backbone_weights)
+    model = build_multitudinous(config.img_backbone, config.point_cloud_backbone, config.neck, config.head, args.img_backbone_weights, args.point_cloud_backbone_weights)
 
     # transfer the model to the cpu
     model.to(device)
@@ -40,8 +40,8 @@ if __name__ == "__main__":
     # TODO
 
     # generate random input tensors
-    random_pcl = torch.rand((1, 10000)).to(device)
-    random_rgbd = torch.rand((1, 4, 224, 224)).to(device)
+    random_pcl = torch.rand((2, 6000, 3)).to(device)
+    random_rgbd = torch.rand((2, 4, 704, 1280)).to(device)
 
     # test forward pass with random tensors (with correct dimensions)
     out = model(random_pcl, random_rgbd)
